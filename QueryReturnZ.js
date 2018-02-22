dc48d99ebbdfdda2191706b27ce7b770712a9e29
mtrsklnkv <mtrsklnkv@gmail.com>
2018-02-22 11:31:38 +0000

    first commit2

---
+++ QueryReturnZ.js
@@ -0,0 +1,14 @@
+﻿require(["esri/tasks/query"], function(ll)
+{
+	if( typeof esri.tasks.Query.prototype.toJson == 'function' )
+	{
+		esri.tasks.Query.prototype.toJson2 = esri.tasks.Query.prototype.toJson;
+		esri.tasks.Query.prototype.toJson = function(f)
+		{
+			var myJson = this.toJson2(f);
+			myJson.returnZ = true;
+			console.log(myJson)
+			return myJson;
+		}
+	}
+});
